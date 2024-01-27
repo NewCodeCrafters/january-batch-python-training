3e4e2b09a8f99f97cbca2737ef3ce028880b7611
slime42 <divineanalo@yahoo.com>
2024-01-27 11:33:35 +0100

    string_concat

---
+++ divine.py
@@ -0,0 +1,18 @@
+# I am Divine. I love coding and my intructor is the best in town.
+aph1 = "I "
+aph2 = "am "
+aph3 = "Divine. "
+aph4 = "I "
+aph5 = "love "
+aph6 = "coding "
+aph7 = "and "
+aph8 = "my "
+aph9 = "intructor "
+aph10 = "is "
+aph11 = "the "
+aph12 = "best "
+aph13 = "in "
+aph14 = "town."
+
+sentence = aph1 + aph2 + aph3 + aph4 + aph5 + aph6 + aph7 + aph8 + aph9 + aph10 + aph11 + aph12 + aph13 + aph14
+print(sentence)
