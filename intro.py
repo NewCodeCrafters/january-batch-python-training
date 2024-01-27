089d94c4a8d5ee247d2176aa5b5b578ff3be373e
cyber-spencer <ifeanyichukwu20204@gmail.com>
2024-01-27 11:43:13 +0100

    my program

--- intro.py
+++
@@ -1,4 +0,0 @@
-print(30 + 20)
-print("Introduction")
-print("Hello Tutor")
-print(10+10)
--- spencer.py
+++ spencer.py
@@ -1,4 +1,8 @@
-hello am spencer
-how are you doingam just tired
-what happen to you
-been at home since
+alph1 = "vegetable "
+alph2 = "is "
+alph3 = "a "
+alph4 = "very "
+alph5 = "good "
+alph6 = "fruit. "
+sentence = alph1 + alph2 + alph3 + alph4 + alph5 + alph6
+print(sentence) 
