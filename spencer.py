e92adf7185da8351e822bd7be370e32574987620
cyber-spencer <ifeanyichukwu20204@gmail.com>
2024-01-30 10:44:18 +0100

    strings classification

--- spencer.py
+++ spencer.py
@@ -1,8 +1,13 @@
-alph1 = "vegetable "
-alph2 = "is "
-alph3 = "a "
-alph4 = "very "
-alph5 = "good "
-alph6 = "fruit. "
-sentence = alph1 + alph2 + alph3 + alph4 + alph5 + alph6
-print(sentence) 
+I NEED TO LEARN MORE ABOUT CYBER SECURITY MOST ESPECIALLY HACKING.
+
+alfa1 "i  "
+alfa2 "need "
+alfa3 "to "
+alfa4 "learn "
+alfa5 "more "
+alfa6 "about "
+alfa7 "cyber "
+alfa8 "security "
+alfa9 "most "
+alfa10 "especially "
+alfa11 "hacking. "
