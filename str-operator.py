e6e64f7397b66f94ad16a3198f6f6df8155f1aef
cyber-spencer <ifeanyichukwu20204@gmail.com>
2024-01-30 10:51:38 +0100

    str-operator

---
+++ str-operator.py
@@ -0,0 +1,26 @@
+# strings operators
+gender = "A boy won a powerball lottery last weekend"
+fantasy = "I heard the moon cries when it losses it's shape."
+country = "Canada citizenship is easily acquired. "
+holiday = "I love taking my vacations abroad. "
+weather = "It's rainning cat and dog out there"
+continent = "Nigeria is the giant of Africa"
+fruit1 = "Orange is a sweet fruit"
+instinct = "Joy gladdens my heart"
+num3 = 34
+num4 = 42
+"34" + "42" = "3442"
+str(True) = "True"
+
+total = str(num3) + str(num4)
+print(total)
+
+# upper(), lower(), capitalize() and title() method operator
+print(gender.upper())
+print(continent.lower())
+print(fantasy.capitalize())
+print(holiday.title())
+
+# Length of a string => len()
+
+print(len(weather))
