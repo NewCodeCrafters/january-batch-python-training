b3ac96fd5d72d3c0cf6d2699921650266b740e66
cyber-spencer <ifeanyichukwu20204@gmail.com>
2024-02-03 11:04:03 +0100

    string-conversion

---
+++ string-conversion.py
@@ -0,0 +1,39 @@
+# Python strings
+
+# len(), upper(), "" or '', lower(), title(), capitalize()
+
+# var1 = "i am a boy"
+
+# print(var1.capitalize())
+# print(var1.title())
+# print("I am Bello AbdulHakeem.")
+
+# input()
+
+# name = input("What is your name? ")
+# sentence = f"Hello {name.title()}. Welcome to python."
+# print(sentence)
+
+
+
+# You are to take user's favorite drink inputs. And print out the statement the following way
+
+# This is very nice. I didn't know you like Fanta.
+
+# drink = input("what drink do you love most? ")
+# sentence = f"this is very nice. i didn't know you like {name.title()}"
+
+# I want you to ask for the user's Age and print out the following statement
+
+# Wow! I didn't know you are 23 years old
+
+# age = input("how old are you? ")
+# sentence = f"I'm indeed suprise. you are {age} years old."
+# print(sentence)
+
+# input str "2000" 2024 2000
+birth_year = input("What year were you born? ")
+age = 2024 - int(birth_year)
+reply = f"You are born in {birth_year}. You are {age} years old."
+print(reply)
+# You are born in 1992. You are 32 years old
