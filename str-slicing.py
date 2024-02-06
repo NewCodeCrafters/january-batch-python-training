656f67067ce0d635292dd823e5b698ba0861b0a7
officialalkenes <belloabdulhakeemolamide@gmail.com>
2024-02-06 11:14:49 +0100

    String Slicing and Indexing

---
+++ str-slicing.py
@@ -0,0 +1,14 @@
+# Strings Indexing and slicing
+
+food = "Amala is almost ready"
+
+print(food[-3])
+
+print(food[0:3])
+print(food[9:-6])
+print(food[9:15])
+# I was able to slice amala from the sentence and this is the output
+
+statement = f"I was able to slice {food[0:5]} from the sentence and this is the output"
+
+print(statement)
