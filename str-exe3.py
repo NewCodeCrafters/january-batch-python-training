a2a04d82e27f698f7b8ec6be8f3f4e38714d349b
cyber-spencer <ifeanyichukwu20204@gmail.com>
2024-02-06 11:57:35 +0100

    str-exe3

---
+++ str-exe3.py
@@ -0,0 +1,7 @@
+sport_activity = "I love playing football as a left winger"
+
+# my favorite sport while _____ is playing as a _______
+
+statement = f"My favourite sport in {sport_activity[0:14]} as a {sport_activity[0:40]}"
+
+print(statement)
