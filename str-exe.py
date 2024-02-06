f79f54da029768cd824f7a04ed22f39d0e1275d8
dev-raji <dosky105@gmail.com>
2024-02-06 11:20:30 +0100

    furtherstringsliing

---
+++ str-exe.py
@@ -0,0 +1,6 @@
+profession = "I am a senior lecturer at LASUED. I major in Mathematics Science."
+
+# I highlighted my profession as a _______. I also listed my major which is _______
+
+sentence = f"I highlighted my profession as a {profession[14:22]}. I also listed my major which is {profession[45:65]}."
+print(sentence)
