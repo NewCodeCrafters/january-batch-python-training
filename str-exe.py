dabd4281d164ea8d0f91dcade18256fa3cc881b0
slime42 <divineanalo@yahoo.com>
2024-02-06 11:23:07 +0100

    string exercise

---
+++ str-exe.py
@@ -0,0 +1,7 @@
+sentence = "Hi, I am Divine Analo. A machine Learning expert"
+
+# I got my name with string slicing and the name is ____
+
+statement = f"i got my name with string slicing and the name is {sentence[8:22]}"
+
+print(statement)
