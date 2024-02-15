733aff9fd80bc7da85aedbcbde9743d9d2ff7cc4
cyber-spencer <ifeanyichukwu20204@gmail.com>
2024-02-15 13:27:28 +0100

    list.py

---
+++ listexe.py
@@ -0,0 +1,13 @@
+
+
+fruits = ["Mango", "Orange", "Watermelon", "Pineapple", "Pawpaw", "Cherry", "Berries", "Date", "Plum", "Soursop"]
+
+# print Berries only
+# print the following Mango, Pineapple, Berries, Soursop
+# print the following Berries Date Plum
+# print the following Cherry Date Soursop
+# Bonus: print the following Soursop Date Cherry Pineapple Orange
+
+print(fruits[6])
+print(fruits[ :18:3])
+print(fruits[6:7:8])
