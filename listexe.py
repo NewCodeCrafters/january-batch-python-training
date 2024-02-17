d8bb75f69bf4c166edbba2270f7da449fd52d0b0
dev-raji <dosky105@gmail.com>
2024-02-17 09:41:39 +0100

    listcorrection.py

---
+++ listexe.py
@@ -0,0 +1,37 @@
+# Python DataTypes Lists
+
+# student1 = "Mrs. Raji"
+# student2 = "Mr. Spencer"
+# student3 = "Mr. Divine"
+
+# print(student1)
+
+# students = ["Mr. Spencer", "Mrs. Raji", "Mr.Divine"]
+# print(students)
+# # Lists are orderable
+# student1 = ["Spencer", "Male", "Cyber Security", 1]
+# student2 = ["Mrs. Raji", "Female", "Machine Learning", 2]
+
+# print(student1)
+# # Lists are arbitrary => 
+
+# # indexing and slicing
+
+groceries = ["sugar", "Maggi", "salt", "Apples", "Garri", "Beans", "Cereals", "Drinks"]
+# sugar, salt and Garri 
+
+# # beans cereals and drinks
+
+# # salt garri and cereals
+
+print(groceries[:5:2])
+print(groceries[5:])
+print(groceries[2:7:2])
+
+fruits = ["Mango", "Orange", "Watermelon", "Pineapple", "Pawpaw", "Cherry", "Berries", "Date", "Plum", "Soursop"]
+
+# print Berries only
+# print the following Mango, Pineapple, Berries, Soursop
+# print the following 
+# 
+# 
